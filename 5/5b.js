2c2e76a0fa7a4e04778a61d9229a8e105d8bc08f
BorislavBranimirov <boriintorr@gmail.com>
2023-12-06 00:59:42 +0000

    5

--- 5/5b.js
+++ 5/5b.js
@@ -30,75 +30,34 @@ for (const section of sections) {
   }
 
   const newIdRanges = [];
-  for (let j = 0; j < idRanges.length; j++) {
+  for (const idRange of idRanges) {
     let matched = false;
     for (const idMapping of idMap) {
-      if (
-        idMapping.start <= idRanges[j][0] &&
-        idMapping.end >= idRanges[j][1]
-      ) {
-        // mapping covers the range
-        newIdRanges.push([
-          idRanges[j][0] + idMapping.diff,
-          idRanges[j][1] + idMapping.diff,
-        ]);
-        matched = true;
-        break;
-      }
+      if (idMapping.start <= idRange[1] && idMapping.end >= idRange[0]) {
+        // mapping has at least one end inside the range
+        if (idMapping.start > idRange[0]) {
+          // range on the left of mapping
+          idRanges.push([idRange[0], idMapping.start - 1]);
+        }
+        if (idMapping.end < idRange[1]) {
+          // range on the right of mapping
+          idRanges.push([idMapping.end + 1, idRange[1]]);
+        }
 
-      let extractStart = null;
-      let extractEnd = null;
-      if (
-        idMapping.start >= idRanges[j][0] &&
-        idMapping.start <= idRanges[j][1]
-      ) {
-        extractStart = idMapping.start;
-      }
-      if (idMapping.end >= idRanges[j][0] && idMapping.end <= idRanges[j][1]) {
-        extractEnd = idMapping.end;
-      }
-
-      if (extractStart !== null && extractEnd !== null) {
-        // mapping is within the range
+        // mapping within the range
+        let extractStart = Math.max(idRange[0], idMapping.start);
+        let extractEnd = Math.min(idRange[1], idMapping.end);
         newIdRanges.push([
           extractStart + idMapping.diff,
           extractEnd + idMapping.diff,
         ]);
-        if (idRanges[j][1] - extractEnd !== 0) {
-          idRanges.push([extractEnd + 1, idRanges[j][1]]);
-        }
-        if (extractStart - idRanges[j][0] !== 0) {
-          idRanges.push([idRanges[j][0], extractStart - 1]);
-        }
-        matched = true;
-        break;
-      } else if (extractStart !== null) {
-        // mapping's start is within the range and end is outside it
-        newIdRanges.push([
-          extractStart + idMapping.diff,
-          idRanges[j][1] + idMapping.diff,
-        ]);
-        if (extractStart - idRanges[j][0] !== 0) {
-          idRanges.push([idRanges[j][0], extractStart - 1]);
-        }
-        matched = true;
-        break;
-      } else if (extractEnd !== null) {
-        // mapping's end is within the range and start is outside it
-        newIdRanges.push([
-          idRanges[j][0] + idMapping.diff,
-          extractEnd + idMapping.diff,
-        ]);
-        if (idRanges[j][1] - extractEnd !== 0) {
-          idRanges.push([extractEnd + 1, idRanges[j][1]]);
-        }
         matched = true;
         break;
       }
     }
     if (!matched) {
       // mapping is outside the range
-      newIdRanges.push(idRanges[j]);
+      newIdRanges.push(idRange);
     }
   }
   idRanges = newIdRanges;
